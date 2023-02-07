e58aed2dbf81ae2e60ee8c4e25a0f28a60887d55
Charley Liu <charleyliu716@gmail.com>
2023-02-07 18:40:05 -0500

    Removed some links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,10 +16,8 @@ function Navbar() {
                     </li>
 
                     <li><a href="" className="text-link">About</a></li>
-                    <li><a href="" className="text-link">Skills</a></li>
                     <li><a href="" className="text-link">Personal Projects</a></li>
-                    <li><a href="" className="text-link">Contacts</a></li>
-                    <li><a href="/src/data/CharleyLiu_Resume.pdf" className="text-link" download>Resume</a></li>
+                    <li><a href="/src/data/CharleyLiu_Resume.pdf" className="text-link resume" download>Resume</a></li>
                 </ul>
             </nav>
         </header>
