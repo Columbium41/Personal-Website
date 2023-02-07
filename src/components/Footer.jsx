df4342f5171322f378f19a6bfba0be2fc10d8d0f
Charley Liu <charleyliu716@gmail.com>
2023-02-07 18:37:56 -0500

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p>Created by Charley Liu - View&nbsp;
+                <a href="https://github.com/Columbium41/Personal-Website" target="_blank">Source Code</a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
