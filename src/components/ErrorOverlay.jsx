f410f1272245cc447f458ff4393ae2c0d87d9f4b
Charley Liu <charleyliu716@gmail.com>
2023-02-10 09:35:26 -0500

    Added error overlay styles

--- src/components/ErrorOverlay.jsx
+++ src/components/ErrorOverlay.jsx
@@ -1,7 +1,8 @@
 function ErrorOverlay() {
     return (
         <div className="error-overlay">
-            error
+            <h2>Error: Page not loading correctly</h2>
+            <p>Please refresh the page and try again later</p>
         </div>
     )
 }
