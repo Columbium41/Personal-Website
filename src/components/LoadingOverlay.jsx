3b76c24f6283a879b993dbf214b22dd828c6033c
Charley Liu <charleyliu716@gmail.com>
2023-02-10 09:31:20 -0500

    Added loading overlay styles

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 function LoadingOverlay() {
+    const [loadingText, setLoadingText] = useState('Loading.');
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (count === 3) {
+                setCount(0);
+                setLoadingText("Loading.");
+            }
+            else {
+                setCount(count + 1);
+                setLoadingText(loadingText + ".");
+            }
+        }, 500);
+    }, [loadingText])
+
     return (
         <div className="loading-overlay">
-            loading
+            <img src="/src/assets/react.svg" alt="React logo" />
+            <h2>{ loadingText }</h2>
         </div>
     )
 }
