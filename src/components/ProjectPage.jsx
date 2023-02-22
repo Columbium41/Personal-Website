7e9a83c38e62936e57d225724099976bc07f12da
Charley Liu <charleyliu716@gmail.com>
2023-02-22 13:16:47 -0500

    Added 404 pages for project pages

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,5 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import NotFound from "./NotFound";
 
 function ProjectPage({ data, setDocumentTitle }) {
     const params = useParams();
@@ -15,49 +16,51 @@ function ProjectPage({ data, setDocumentTitle }) {
         }
     }
 
-    console.log(Object.entries(projectData.links));
-
     useEffect(() => {
         setDocumentTitle(projectTitle.replace("_", " "));
     }, [])
 
     return (
-        <div className="project-page">
-            <h3 className="back-to-projects" onClick={() => {navigate("/projects")}}>
-                {'<'} Back to Projects
-            </h3>
+        <div>
+            { (projectData !== "") && 
+            <div className="project-page">
+                <h3 className="back-to-projects" onClick={() => {navigate("/projects")}}>
+                    {'<'} Back to Projects
+                </h3>
 
-            <h2 className="align-center">{ projectTitle.replace("_", " ") }</h2>
-            <div className="text-section">
-                <p>{projectData.long_description}</p>
-            </div>
-
-            <div className="project-metadata-container">
-                <div className="project-metadata">
-                    <h4>Date Created:</h4>
-                    <p>{ projectData.date_created }</p>
+                <h2 className="align-center">{ projectTitle.replace("_", " ") }</h2>
+                <div className="text-section">
+                    <p>{projectData.long_description}</p>
                 </div>
 
-                <div className="project-metadata topics-section">
-                    <h4>Topics:</h4>
-                    {projectData.topics.map((topic) => (
-                        <p className="topic-item" key={topic}>{topic}</p>
+                <div className="project-metadata-container">
+                    <div className="project-metadata">
+                        <h4>Date Created:</h4>
+                        <p>{ projectData.date_created }</p>
+                    </div>
+
+                    <div className="project-metadata topics-section">
+                        <h4>Topics:</h4>
+                        {projectData.topics.map((topic) => (
+                            <p className="topic-item" key={topic}>{topic}</p>
+                        ))}
+                    </div>
+
+                    {Object.entries(projectData.links).map((array, index) => (
+                        <div className="project-metadata" key={index}>
+                            <h4>{ array[0].replace("_", " ") + ":" }</h4>
+                            <p className="break-word">
+                                <a href={ array[1] } target="_blank" >{ array[1] }</a>
+                            </p>
+                        </div>
                     ))}
                 </div>
-
-                {Object.entries(projectData.links).map((array, index) => (
-                    <div className="project-metadata" key={index}>
-                        <h4>{ array[0].replace("_", " ") + ":" }</h4>
-                        <p className="break-word">
-                            <a href={ array[1] } target="_blank" >{ array[1] }</a>
-                        </p>
-                    </div>
+                        
+                {projectData.thumbnails.map((thumbnail, index) => (
+                    <img key={index} src={thumbnail} alt={"thumbnail " + index} className="project-thumbnail" />
                 ))}
-            </div>
-                    
-            {projectData.thumbnails.map((thumbnail, index) => (
-                <img key={index} src={thumbnail} alt={"thumbnail " + index} className="project-thumbnail" />
-            ))}
+            </div>}
+            {(projectData === "") && <NotFound setDocumentTitle={setDocumentTitle} />}
         </div>
     )
 }
