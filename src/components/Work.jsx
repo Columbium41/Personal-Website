3d204b9c5adfc34df2a1eb3706f948e3dec7cf43
Charley Liu <charleyliu716@gmail.com>
2025-04-06 19:38:55 -0400

    new resume

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import WorkCard from './WorkCard';
 function Work({ data }) {
     return (
         <div className="work-experience w-100">
-            <h2 className="section-header">Work</h2>
+            <h2 className="section-header">Work Experience</h2>
             <div className="positions">
                 {data.map((position, index) => (
                     <WorkCard position={position} key={index}/>
